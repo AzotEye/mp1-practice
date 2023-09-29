f249738192314a17d5d30f799ca6c78269d8b8a3
unknown <ITMM-230041@Q113.local>
2023-09-29 14:03:31 +0300

    fixed the Code

--- Sircles/Task1/Code.cpp
+++ Sircles/Task1/Code.cpp
@@ -8,59 +8,40 @@ int main()
     scanf("%lf %lf %lf", &x1, &y1, &r1);
     printf("Input the coordinates of the center of the second circle and its radius: ");
     scanf("%lf %lf %lf", &x2, &y2, &r2);
-    if ((r1 > 0) && (r2 > 0))
+    if ((r1 <= 0) || (r2 <= 0))
     {
-        if ((x1 == x2) && (y1 == y2))
-        {
-            if (r1 == r2)
-            {
-                printf("The circles coincide");//sovpadayt
-            }
-            else
-            {
-                printf("One of the circles lies inside the other");//odin vnutry drugogo
-            }
-        }
-        else
-        {
-            double distance, R, r;
-            if (r1 > r2)
-            {
-                R = r1;
-                r = r2;
-            }
-            else
-            {
-                R = r2;
-                r = r1;//vybrali max i min radiusy
-            }
-            distance = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));//rasstoyanie mezhdy centrami
-            if (distance == r1 + r2)
-            {
-                printf("Circles touch at one point outside");//vnechnee kasanie
-            }
-            else if (distance > r1 + r2)
-            {
-                printf("Circles do not intersect");//ne peresecautca
-            }
-            else if ((distance < r1 + r2) && (distance > R - r))
-            {
-                printf("Circles intersect in two points");//peresecautca v dvyh tochkah
-            }
-            else if (distance < R - r)
-            {
-                printf("One of the circles lies inside the other");//pdin vnytry drygogo
-            }
-            else if (distance == R - r)
-            {
-                printf("Circles touch at one point inside");//kasanie vnutri
-            }
-
-        }
+        printf("incorrect data");
+        return 0;
+    }
+    if ((x1 == x2) && (y1 == y2) && (r1 == r2))
+    {
+        printf("The circles coincide");//sovpadayt
     }
     else
     {
-        printf("Incorrect data!"); //proverca na polozhitelny radius
+        double distance;
+        distance = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));//rasstoyanie mezhdy centrami
+        if (distance == r1 + r2)
+        {
+            printf("Circles touch at one point outside");//vnechnee kasanie
+        }
+        else if (distance > r1 + r2)
+        {
+            printf("Circles do not intersect");//ne peresecautca
+        }
+        else if (distance < fabs(r1 - r2))
+        {
+            printf("One of the circles lies inside the other");//odin vnytry drygogo
+        }
+        else if (distance == fabs(r1 - r2))
+        {
+            printf("Circles touch at one point inside");//kasanie vnutri
+        }
+        else
+        {
+            printf("Circles intersect in two points");//peresecautca v dvyh tochkah
+        }
+
+        return 0;
     }
-    return 0;
 }
