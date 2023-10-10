f30c75ce548594cd892e452054132eca52fc5280
Leonova Anna <leonovaa441@gmail.com>
2023-10-10 20:43:59 +0300

    completed

--- LeonovaAS/Practice1/Wardrobe/Wardrobe.c
+++ LeonovaAS/Practice1/Wardrobe/Wardrobe.c
@@ -1,27 +1,20 @@
 #include <stdio.h>
 int main()
 {
-    double h, w, d, m1, m2, m3, m4, m5, colvopolk, m;
-    printf("Input height, width, depth of the wardrobe in cm: ");
-    scanf("%lf %lf %lf", &h, &w, &d);
-    while ((h < 180) || (h > 220) || (w < 80) || (w > 120) || (d < 50) || (d > 90))
-    {
-        printf("Incorrect! Try again please! Input height, width, depth of the wardrobe in cm: ");
-        scanf("%lf %lf %lf", &h, &w, &d);
-    }
-    if (h < 190)
-    {
+    double h, w, d, m1, m2, m3, m4, m5, colvopolk = 5, m;
+    do {
+        printf("Input height, depth,  width of the wardrobe ");
+        scanf("%lf %lf %lf", &h, &d, &w);
+    } while ((h < 180) || (h > 220) || (w < 80) || (w > 120) || (d < 50) || (d > 90));
+    h -= 3;
+    if (h <= 206) {
         colvopolk = 4;
     }
-    else
-    {
-        colvopolk = 5;
-    }
-    m1 = h * w * 0.5 * 0.8;
-    m2 = 2 * h * d * 1.5 * 0.5;
-    m3 = 2 * w * d * 1.5 * 0.5;
-    m4 = h * h * w * 0.7;
-    m5 = colvopolk * w * d * 1.5 * 0.5;
+    m1 = d * w * 1.5 * 2 * 0.75;
+    m2 = h * d * 1.5 * 2 * 0.75;
+    m3 = h * w * 1.54;
+    m4 = colvopolk * (d - 0.5) * (w - 3) * 0.75;
+    m5 = h * w * 0.5 * 0.85;
     m = (m1 + m2 + m3 + m4 + m5) / 1000;
     printf("Weight of your wardrode in kg: %lf", m);
     return 0;
