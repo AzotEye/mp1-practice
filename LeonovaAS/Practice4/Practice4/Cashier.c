0d1531d40345a87aed04d4f99c4842738afa8bc7
unknown <ITMM-230041@Q113.local>
2023-12-15 13:37:06 +0300

    Replace int by double

--- LeonovaAS/Practice4/Practice4/Cashier.c
+++ LeonovaAS/Practice4/Practice4/Cashier.c
@@ -8,8 +8,8 @@
 
 char* codes[N_PRODUCTS] = { "1000", "1001", "1002", "1003", "1004", "1005", "1006", "1007", "1008", "1009"};
 char* names[N_PRODUCTS] = { "Bread", "Milk", "Candy", "Potato", "Apple", "Juice", "Orange", "Carrot", "Cookie", "Tomato"};
-int discount[N_PRODUCTS] = { 0, 5, 15, 3, 5, 45, 0, 5, 37, 3 },
-price[N_PRODUCTS] = { 41, 69, 59, 14, 20, 119, 58, 9, 79, 25 };
+double discount[N_PRODUCTS] = { 0.0, 5.0, 15.0, 3.0, 5.0, 45.0, 0.0, 5.0, 37.0, 3.0 },
+       price[N_PRODUCTS] = { 41.0, 69.0, 59.0, 14.0, 20.0, 119.0, 58.0, 9.0, 79.0, 25.0 };
 int check[N_PRODUCTS] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
 
@@ -21,13 +21,9 @@ void print_total_info();
 
 int main() 
 {
-    int i;
-    float total_disk = 0, total_price = 0;
-
     enter_barcodes();
     print_check();
     print_total_info();
-  
     return 0;
 }
 
@@ -64,11 +60,9 @@ void enter_barcodes()
         }
 
         check[pidx]++;
-
-
     } while (cont == 1);
-    return;
 }
+
 void print_check()
 {
     int i;
@@ -77,13 +71,13 @@ void print_check()
         if (check[i] != 0)
         {
             printf("Name: %s\n", names[i]);
-            printf("\tPrice: %d. \nDiscount: %d. \nQuantity: %d. \nTotal price: %f\n",
+            printf("\tPrice: %f. \nDiscount: %f. \nQuantity: %d. \nTotal price: %f\n",
                 price[i], discount[i], check[i], price[i] * ((100 - discount[i]) * 0.01) * check[i]);
            
         }
     }
-    return;
 }
+
 void print_total_info()
 {
     float total_disk = 0, total_price = 0;
@@ -97,5 +91,4 @@ void print_total_info()
         }
     }
     printf("\nYour total discount: %f\nYour total price: %f", total_disk, total_price);
-    return;
 }
